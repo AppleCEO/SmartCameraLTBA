878366e587da4b896435a95b467c5c5d076fbe41
도미닉 <kiljh@me.com>
2019-11-01 20:36:30 +0900

    프로젝트 생성

---
+++ SmartCameraLTBA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartCameraLTBA
+//
+//  Created by joon-ho kil on 2019/11/01.
+//  Copyright © 2019 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
